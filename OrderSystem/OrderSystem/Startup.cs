45df0214bdf6e611ec4699dae5a20dff1d25a358
plamengeorgiev <plamen.flame@gmail.com>
2015-11-22 12:57:03 +0200

    Added default Asp.NET MVC application

---
+++ OrderSystem/OrderSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OrderSystem.Startup))]
+namespace OrderSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
